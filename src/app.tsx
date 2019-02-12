b450a076ca8fc6598bb6779556c62dacec12bc3b
Sean Price <seanprice@sevenhillstechnology.com>
2019-02-11 19:05:47 -0500

    Add useMemo

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import {ConnectedComponent1} from './component1';
 import { ConnectedComponent2 } from './component2';
+import { AppContext } from './context/appContext';
+import { useAppContext } from './context/appContextProvider';
 
 export const App = () => {
     console.log("render App");
+    const providerValue = useAppContext();
     return (
-        <ul>
-            <li>
-                <ConnectedComponent1 />
-            </li>
-            <li>
-                <ConnectedComponent2></ConnectedComponent2>
-            </li>
-        </ul>
+        <AppContext.Provider value={providerValue}>
+            <ul>
+                <li>
+                    <ConnectedComponent1 />
+                </li>
+                <li>
+                    <ConnectedComponent2></ConnectedComponent2>
+                </li>
+            </ul>
+        </AppContext.Provider>
     );
 }
--- src/context/connect.tsx
+++ src/context/connect.tsx
@@ -7,33 +7,34 @@ export type Dispatch = (callback: (oldState: State) => State) => void;
 
 
 export const connect = <StateProps, ActionProps, ComponentProps>(mapStateToProps: (state: State) => StateProps, mapActionsToProps: (dispatch: Dispatch) => ActionProps) => {
+    let state: State = null;
 
     return (Component: React.FunctionComponent<ComponentProps>): React.FunctionComponent<ComponentProps> => { 
         const Connect = (props: ComponentProps) =>  {
-
-            const providerValue = useAppContext();
-            console.log(`render connect ${name}`, providerValue.state);
+            const name = Component.displayName || Component.name;
+            const providerValue = useContext(AppContext);
+            state = providerValue.state;
+            
+            console.log(`render connect(${name})`, providerValue.state);
         
             const dispatch: Dispatch = (callback: (oldState: State) => State) => {
                 console.log("dispatch oldState", providerValue.state);
-                const newState = callback(providerValue.state);
+                const newState = callback(state);
                 providerValue.updateState(newState);
             }
     
             const stateAugmentedProps = mapStateToProps(providerValue.state);
             const actionAugmentedProps = mapActionsToProps(dispatch);
 
-            return (
+            return useMemo(() => (
                 /*
                     https://reactjs.org/docs/context.html#contextprovider
                     All consumers that are descendants of a Provider will re-render whenever the Provider’s value prop changes.
                     The propagation from Provider to its descendant consumers is not subject to the shouldComponentUpdate
                     method, so the consumer is updated even when an ancestor component bails out of the update.
                 */
-                <AppContext.Provider value={providerValue}>
-                    <Component {...props} {...stateAugmentedProps} {...actionAugmentedProps} />
-                </AppContext.Provider>
-            )
+                <Component {...props} {...stateAugmentedProps} {...actionAugmentedProps} />
+            ), [...Object.values(props), ...Object.values(stateAugmentedProps)]);
         };
         Connect.displayName = `connect(${Component.displayName || Component.name})`;
         return Connect;
